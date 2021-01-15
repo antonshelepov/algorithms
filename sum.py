f512d791ad5204cf921e926b374a1af0e9a6d99e
antonshelepov <a.shelepov@gmail.com>
2021-01-15 21:24:39 +0100

    sum added

---
+++ sum.py
@@ -0,0 +1,16 @@
+def sum_1(n):
+
+    """
+    this method returns the sum from 0 to n
+    """
+    sum = 0
+    for x in xrange(n+1):
+        sum +=x
+
+    return sum
+
+def sum_2(n):
+    """
+    this method returns the sum from 0 to n
+    """
+    return (n*(n+1))/2
