c8b0fc4340e3415fadd48e1f6d11d2f76cf60f2d
antonshelepov <a.shelepov@gmail.com>
2021-01-25 01:32:05 +0100

    not ready

--- array_pair_sum.py
+++ array_pair_sum.py
@@ -1,6 +1,26 @@
 def pair_sum(arr, k):
+    """
+    :type array: arr
+    :type int: k
+    :rtype int
+    """
+    if len(arr) < 2:
+        return
 
-    pass
+    checked = set()
+    res = set()
+
+    for n in arr:
+        
+        target = k-n
+
+        if target not in checked:
+            checked.add(n)
+
+        else:
+            res.add( (min(n,target)), (max(n,target)) )
+
+    return len(res)
 
 
 if __name__ == "__main__":
