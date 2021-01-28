2f15ee540bb8f74f769b82ef38fb2b266fd83b02
Anton Schelepov <a.schelepov@we22.com>
2021-01-28 23:54:46 +0100

    another def added

--- finder.py
+++ finder.py
@@ -1,4 +1,5 @@
 from nose.tools import assert_equal
+import collections
 
 def finder(arr1, arr2):
 
@@ -7,10 +8,26 @@ def finder(arr1, arr2):
 
     for n1, n2 in zip(arr1, arr2):
         if n1 != n2:
+
             return n1
 
-    return arr1[-1]
+    # return arr1[-1]
 
+def finder2(arr1, arr2):
+    """this method uses a hashtable to store number of times each elem appears in the second array. 
+    Afterwards the counter for this particular element will be decremented by one if found in array1
+    """
+    d=collections.defaultdict(int)
+
+    for n in arr2:
+        d[n] +=1
+
+    for n in arr1:
+        if d[n] == 0:
+            return n
+        
+        else:
+            d[n] -=1
 
 if __name__ == "__main__":
 
@@ -21,10 +38,10 @@ if __name__ == "__main__":
         
         def test(self,sol):
             assert_equal(sol([5,5,7,7],[5,7,7]),5)
-            assert_equal(sol([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
+            # assert_equal(sol([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
             assert_equal(sol([9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1]),6)
             print('ALL TEST CASES PASSED')
 
     # Run test
     t = TestFinder()
-    t.test(finder)
+    t.test(finder2)
