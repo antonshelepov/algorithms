1abd5ad9eea339c9f1629891039c840de056ea2e
Anton Schelepov <a.schelepov@we22.com>
2021-01-29 01:20:49 +0100

    corrected

--- finder.py
+++ finder.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         
         def test(self,sol):
             assert_equal(sol([5,5,7,7],[5,7,7]),5)
-            # assert_equal(sol([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
+            assert_equal(sol([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
             assert_equal(sol([9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1]),6)
             print('ALL TEST CASES PASSED')
 
