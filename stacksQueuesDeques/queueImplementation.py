86888688dca01c7b053d823b4bced5ffa02b5612
antonshelepov <a.shelepov@gmail.com>
2021-02-01 00:43:18 +0100

    corrected

--- stacksQueuesDeques/queueImplementation.py
+++ stacksQueuesDeques/queueImplementation.py
@@ -6,21 +6,28 @@ class Queue(object):
         self.items = []
 
     def isEmpty(self):
-        return self.items = []
+        return self.items == []
 
     def enqueue(self,item):
         self.items.insert(0,item)
 
     def dequeue(self):
-        return self.itemspop()
+        return self.items.pop()
 
     def size(self):
         return len(self.items)
+
+    def show(self):
+        return self.items
 
 q = Queue()
 
 print(q.size())
 q.enqueue(1)
 q.enqueue(5)
+q.enqueue(8)
+q.enqueue(3)
 
 print(q.size())
+q.dequeue()
+print(q.show())
