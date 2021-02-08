939dd963c7fe52b44fa2bfbf3813bc328d6609d3
antonshelepov <a.shelepov@gmail.com>
2021-02-08 22:35:44 +0100

    returns nth to last node

---
+++ linkedLists/linkedListNth2LastNode.py
@@ -0,0 +1,11 @@
+# this func takes a head node and an integer value 'n' and returns nth to last
+# node in the linked list
+
+class Node:
+
+    def __init__(self,value):
+        self.value = value
+        self.nextnode = None
+
+
+
