94123dc60016ba3b8c5a73e0b45b6fd5f2886f53
antonshelepov <a.shelepov@gmail.com>
2021-02-08 23:32:04 +0100

    added 2 problems

--- recursion/intro.py
+++ recursion/intro.py
@@ -1,4 +1,6 @@
 def fact(n):
+    """this method returns factorial of n (n!)
+    """
 
     if n == 0:
         return 1
@@ -6,4 +8,13 @@ def fact(n):
     else:
         return n * fact(n-1)
 
-print(fact(5))
+def sum_func(n):
+    """this method returns the sum of all the individual digits in that integer
+    """
+    if (n<10):
+        return n
+    else:
+        return n%10 + sum_func(int(n/10))
+    pass
+
+
