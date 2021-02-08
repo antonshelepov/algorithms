f32776984fe4957a2417a6d54522d770a62f9d05
antonshelepov <a.shelepov@gmail.com>
2021-02-08 22:30:25 +0100

    made

--- linkedLists/linkedListReversal.py
+++ linkedLists/linkedListReversal.py
@@ -1,11 +1,28 @@
-# 
+#  
 
 class Node(object):
 
     def __init__(self,value):
 
-        self.value = None
+        self.value = value
         self.nextnode = None
+
+def reverse(head):
+
+    current = head
+    prev = None
+    nextnode = None
+
+    while current:
+        
+        nextnode = current.nextnode
+
+        current.nextnode = prev
+
+        prev = current
+        current = nextnode
+
+    return prev
 
 a = Node(1)
 b = Node(2)
@@ -16,4 +33,8 @@ a.nextnode = b
 b.nextnode = c
 c.nextnode = d
 
+print(a.nextnode.value)
+
 reverse(a)
+
+print(d.nextnode.value)
